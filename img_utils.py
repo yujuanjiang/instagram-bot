6143e1efb53f0dac3d85e170b5c7787d175ce77a
Maja <majabojarska98@gmail.com>
2019-02-20 18:11:08 +0100

    probability(val) method takes in [0;1.0] fraction now, format code

--- img_utils.py
+++ img_utils.py
@@ -1,17 +1,20 @@
-from PIL import ImageOps, Image, ImageFilter
 import random
+from PIL import ImageOps, ImageFilter, Image
 
-def probability(amount):
-    if amount > 100:
-        amount = 100
-    if random.randrange(1, 101) <= amount:
+
+def probability(value):
+    if value > 1.0:
+        value = 1.0
+    if random.randrange(1, 101) / 100.0 <= value:
         return True
     return False
+
 
 def get_rand_hex_color():
     hex_color = "#{:06x}".format(random.randint(0, 0xffffff))
     print("Generated HEX color: {}".format(hex_color))
     return hex_color
+
 
 def process_image(image):
     if probability(85):
@@ -24,15 +27,15 @@ def process_image(image):
     #     image = ImageOps.flip(image)
     # if with_probability(50):
     #     image = ImageOps.mirror(image)
-    if probability(50):
+    if probability(0.5):
         posterize_bits = random.randrange(1, 8)
         image = ImageOps.posterize(image, posterize_bits)
-    if probability(75):
+    if probability(0.5):
         threshold = random.randrange(0, 255)
         image = ImageOps.solarize(image, threshold)
-    if probability(50):
+    if probability(0.5):
         image = ImageOps.equalize(image)
-    if probability(50):
+    if probability(0.5):
         blur_radius = random.randrange(0, 3)
         image = image.filter(ImageFilter.GaussianBlur(blur_radius))
 
